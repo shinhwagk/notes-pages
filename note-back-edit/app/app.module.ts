0d8ae82e1035dce9eceb1c8c89ab727aa1a1ffad
gk <shanghai_sl@hotmail.com>
2016-09-20 14:04:45 +0800

    save note

--- note-back-edit/app/app.module.ts
+++ note-back-edit/app/app.module.ts
@@ -6,14 +6,12 @@ import {BrowserModule} from "@angular/platform-browser";
 import {HttpModule} from "@angular/http";
 import {FormsModule} from "@angular/forms";
 import {AppComponent} from "./app.component";
-import {TemplateLabelComponent} from "./template/label.component";
 import {TemplateModifyComponent} from "./template/modify.component";
 import {TemplateAddCommponent} from "./template/add.component";
 
 @NgModule({
   imports: [BrowserModule, HttpModule, FormsModule],
   declarations: [AppComponent,
-    TemplateLabelComponent,
     TemplateModifyComponent,
     TemplateAddCommponent],
   bootstrap: [AppComponent]
