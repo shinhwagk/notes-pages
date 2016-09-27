a851cc27be9d45cebac6cc60ee95df7a5e663759
gk <shanghai_sl@hotmail.com>
2016-09-27 18:04:15 +0800

    save

--- note-back/app/api.services.ts
+++ note-back/app/api.services.ts
@@ -1,9 +1,9 @@
 /**
  * Created by zhangxu on 2016/8/17.
  */
-import {Injectable} from "@angular/core";
-import {Http, Response, Headers, RequestOptions} from "@angular/http";
-import {UrlServices, Urls} from "./url.service";
+import { Injectable } from "@angular/core";
+import { Http, Response, Headers, RequestOptions } from "@angular/http";
+import { UrlServices, Urls } from "./url.service";
 
 @Injectable()
 export class ApiServices {
@@ -24,6 +24,10 @@ export class ApiServices {
     return this._http.get(this._urlServices.noteUrl(id)).map((res: Response) => res.json());
   }
 
-  headers = new Headers({'Content-Type': 'application/json'});
-  options = new RequestOptions({headers: this.headers});
+  docTest(url: string) {
+    return this._http.get(url).map((res: Response) => res.status)
+  }
+
+  headers = new Headers({ 'Content-Type': 'application/json' });
+  options = new RequestOptions({ headers: this.headers });
 }
--- note-back/app/template/relation.component.ts
+++ note-back/app/template/relation.component.ts
@@ -1,24 +1,37 @@
 /**
  * Created by zhangxu on 2016/9/12.
  */
-import {Component, Input} from "@angular/core";
-import {ApiServices} from "../api.services";
+import { Component, Input } from "@angular/core";
+import { ApiServices } from "../api.services";
 
 @Component({
   selector: 'nb-app-note-template-relation',
   templateUrl: `app/template/relation.component.html`,
-  styleUrls: ["app/template/relation.component.css"]
-  // ,
-  // providers: [ApiServices]
+  styleUrls: ["app/template/relation.component.css"],
+  providers: [ApiServices]
 })
 
 export class RelationTemplateComponent {
-  // constructor(private _api: ApiServices) {
-  // }
+  constructor(private _api: ApiServices) {
+  }
 
   @Input() set set_id(id: number) {
     this._id = id
+    this.doc_exist()
   }
 
   _id: number
+  docStatus: boolean = true
+
+  getUrl() {
+    return "https://github.com/shinhwagk/note-back/tree/master/notes/" + this._id
+  }
+
+  doc_exist() {
+    console.info(this.getUrl(), status)
+    this._api.docTest(this.getUrl()).toPromise().then(status => {
+      console.info(this.getUrl(), status)
+      this.docStatus = status == 404 ? false : true
+    })
+  }
 }
